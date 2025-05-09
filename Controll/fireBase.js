6420fa7021b60e950d4fdd0ac0cedf51076f40ba
Kalana-maduranga001 <kalanamadu2000@gmail.com>
2025-05-09 12:28:03 +0530

    Add controller files

---
+++ Controll/fireBase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.7.1/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAbeVEL0Ng63Q3iRRR5bFSiJ0f2oYXWVv0",
+    authDomain: "pos-system-coffee-shop.firebaseapp.com",
+    projectId: "pos-system-coffee-shop",
+    storageBucket: "pos-system-coffee-shop.firebasestorage.app",
+    messagingSenderId: "477286329184",
+    appId: "1:477286329184:web:22a798a12eee996f334c05"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
